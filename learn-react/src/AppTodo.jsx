d98d959bd713ab535bf990ccb89914daeaf23ef0
yhj <dbsguswl4117@naver.com>
2024-12-25 22:10:19 +0900

    [241225]useImmerReducer 사용

--- learn-react/src/AppTodo.jsx
+++ learn-react/src/AppTodo.jsx
@@ -1,12 +1,12 @@
-import React, { useState,useReducer }from 'react';
+import React, { useState }from 'react';
 import './App.css';
 import TodoList from './components/todo/TodoList';
 import todoReducer from './components/reducer/todo-reducer';
-
+import {useImmerReducer} from 'use-immer';
 
 function AppTodo(props) {
   const [todoText, setTodoText] = useState('');
-  const [todos, dispatch] = useReducer(todoReducer, [
+  const [todos, dispatch] = useImmerReducer(todoReducer, [
     { id : 0, text: 'HTML&CSS 공부하기', done:false},
     { id : 1, text: '자바스크립트 공부하기', done:false}
   ]);
--- learn-react/src/components/reducer/todo-reducer.jsx
+++ learn-react/src/components/reducer/todo-reducer.jsx
@@ -1,38 +1,28 @@
-export default function todoReducer(todos, action) {
+export default function todoReducer(draft, action) {
   //type
   switch(action.type){
     case 'added' : {
       const {nextId, todoText} = action; 
-      return [
-        ...todos,
-        {id: nextId, text: todoText, done:false}
-      ];
+      draft.push({id: nextId, text: todoText, done:false});
+      break;
     }
     case 'added_index' : {
       const {nextId, insertAt, todoText} = action;
-      return [
-        ...todos.slice(0, insertAt),
-        {id: nextId, text:todoText, done:false},
-        ...todos.slice(insertAt)
-      ];
+      draft.splice(insertAt,0,{id: nextId, text:todoText, done:false});
+      break;
     }
     case 'reverse' : {
-      return todos.toReversed();
+      return draft.toReversed();
     }
     case 'done' : {
       const {id, done} = action;
-      return [
-        todos.map(item => {
-          if(item.id === id){
-            return {...item, done};
-          }
-          return item;
-        })
-      ]
+      const target = draft.find(item => item.id === id);
+      target.done = done;
+      break;
     }
     case 'delete' : {
       const {deleteId} = action;
-      return todos.filter(item => item.id !== deleteId);
+      return draft.filter(item => item.id !== deleteId);
     }
     default : {
       throw Error('알 수 없는 액션 타입 : ' + action.type);
