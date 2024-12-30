d32cc519cf70a58424ec7311791f623bd7c338cd
yhj <dbsguswl4117@naver.com>
2024-12-30 15:44:36 +0900

    [241230] style-module.css 개념

---
+++ lean-canvas-react/src/components/Card1.jsx
@@ -0,0 +1,6 @@
+import styles from './Card1.module.css';
+
+function Card1() {
+  return <article className={styles.card}>Card1</article>;
+}
+export default Card1;
---
+++ lean-canvas-react/src/components/Card2.jsx
@@ -0,0 +1,6 @@
+import styles from './Card2.module.css';
+
+function Card2() {
+  return <article className={styles.card}>Card2</article>;
+}
+export default Card2;
